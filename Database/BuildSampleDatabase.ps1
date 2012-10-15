ab681fc4298e0f17489abbe2c6d8a53af612a253
Eli Weinstock-Herman <tarwn@tiernok.com>
2012-10-14 22:27:33 -0400

    Paramaterized sample data script, addition of admin user

--- Database/BuildSampleDatabase.ps1
+++ Database/BuildSampleDatabase.ps1
@@ -1,38 +1,76 @@
-Add-PSSnapin SqlServerCmdletSnapin100
-Add-PSSnapin SqlServerProviderSnapin100
+Param(
+    [parameter(Mandatory=$true)]
+    [alias("s")]
+    $Server,
+    [parameter(Mandatory=$true)]
+    [alias("d")]
+    $Database,
+    [parameter(Mandatory=$true)]
+    [alias("nu")]
+    $NewUserName,
+    [parameter(Mandatory=$true)]
+    [alias("np")]
+    $NewPassword,
+    [parameter(Mandatory=$true)]
+    [alias("au")]
+    $AdminUserName,
+    [parameter(Mandatory=$true)]
+    [alias("ap")]
+    $AdminPassword)
+    
+if ( (Get-PSSnapin -Name SqlServerCmdletSnapin100 -ErrorAction SilentlyContinue) -eq $null ){
+    Add-PSSnapin SqlServerCmdletSnapin100
+    Add-PSSnapin SqlServerProviderSnapin100
+}
 
 $path = (Get-Location).Path
-$database = "SQLisHard_01"
-$server = "localhost"
-$readuser = "readuser"
-$readpass = "12345"
 
 #database
-Write-Host "Creating Database: $database"
-Invoke-Sqlcmd -Query "CREATE DATABASE $database; ALTER DATABASE $database SET RECOVERY SIMPLE;" -ServerInstance "$server" -Database "master"
-Write-Host "Created."
+try{
+    Write-Host "Creating Database: $database"
+    Invoke-Sqlcmd -Query "CREATE DATABASE $database; ALTER DATABASE $database SET RECOVERY SIMPLE;" -ServerInstance "$Server" -Username "$AdminUserName" -Password "$AdminPassword" -Database "master" -ErrorAction Stop
+    Write-Host "Created."
+}
+catch{
+    Write-Error "Script error: $_" -EA Stop
+}
 
 #user
-Write-Host "Creating User: $readuser"
-Invoke-Sqlcmd -Query "IF NOT EXISTS (SELECT 1 FROM master.dbo.syslogins WHERE name = '$readuser') CREATE LOGIN $readuser WITH PASSWORD = '$readpass';" -ServerInstance "$server" -Database "master"
-Invoke-Sqlcmd -Query "EXEC sp_defaultdb @loginame='$readuser', @defdb='$database'; CREATE USER readuser FOR LOGIN readuser WITH DEFAULT_SCHEMA = dbo; EXEC sp_addrolemember 'db_datareader','$readuser'" -ServerInstance "$server" -Database "$database"
-Write-Host "Created."
+try{
+    Write-Host "Creating User: $readuser"
+    Invoke-Sqlcmd -Query "IF NOT EXISTS (SELECT 1 FROM master.dbo.syslogins WHERE name = '$NewUserName') CREATE LOGIN $NewUserName WITH PASSWORD = '$NewPassword';" -ServerInstance "$Server" -Username "$AdminUserName" -Password "$AdminPassword" -Database "master" -ErrorAction Stop
+    Invoke-Sqlcmd -Query "EXEC sp_defaultdb @loginame='$NewUserName', @defdb='$Database'; CREATE USER $NewUserName FOR LOGIN $NewUserName WITH DEFAULT_SCHEMA = dbo; EXEC sp_addrolemember 'db_datareader','$NewUserName'" -ServerInstance "$Server" -Username "$AdminUserName" -Password "$AdminPassword" -Database "$Database" -ErrorAction Stop
+    Write-Host "Created."
+}
+catch{
+    Write-Error "Script error: $_" -EA Stop
+}
 
 #numbers table
-Write-Host "Creating Table: dbo.Numbers"
-Invoke-Sqlcmd -InputFile "$path\Data\numbers.sql" -ServerInstance "$server" -Database "$database"
-Invoke-Sqlcmd -Query "SELECT COUNT(*) FROM dbo.Numbers;" -ServerInstance "$server" -Database "$database" -Username "$readuser" -Password "$readpass"
-Write-Host "Created."
+try{
+    Write-Host "Creating Table: dbo.Numbers"
+    Invoke-Sqlcmd -InputFile "$path\Data\numbers.sql" -ServerInstance "$Server" -Username "$AdminUserName" -Password "$AdminPassword" -Database "$Database" -ErrorAction Stop
+    Invoke-Sqlcmd -Query "SELECT COUNT(*) FROM dbo.Numbers;" -ServerInstance "$Server" -Username "$NewUserName" -Password "$NewPassword" -Database "$Database" -ErrorAction Stop
+    Write-Host "Created."
+}
+catch{
+    Write-Error "Script error: $_" -EA Stop
+}
 
 #clients table
-Write-Host "Creating Table: dbo.Clients"
-$old="%PATH%"
-$new="$path\Data\"
-(Get-Content "$path\Data\BulkImportNames.sql") | % {$_ -replace $old, $new} | Set-Content -path "$path\Data\BulkImportNamesRunnable.sql"
-invoke-sqlcmd -inputfile "$path\Data\BulkImportNamesRunnable.sql" -serverinstance "$server" -database "$database" -QueryTimeout 120 
-Invoke-Sqlcmd -Query "SELECT COUNT(*) FROM dbo.Clients;" -ServerInstance "$server" -Database "$database" -Username "$readuser" -Password "$readpass"
-DEL "$path\Data\BulkImportNamesRunnable.sql"
-Write-Host "Created."
+try{
+    Write-Host "Creating Table: dbo.Clients"
+    $old="%PATH%"
+    $new="$path\Data\"
+    (Get-Content "$path\Data\BulkImportNames.sql") | % {$_ -replace $old, $new} | Set-Content -path "$path\Data\BulkImportNamesRunnable.sql"
+    invoke-sqlcmd -inputfile "$path\Data\BulkImportNamesRunnable.sql" -ServerInstance "$Server" -Username "$AdminUserName" -Password "$AdminPassword" -Database "$Database" -QueryTimeout 120  -ErrorAction Stop
+    Invoke-Sqlcmd -Query "SELECT COUNT(*) FROM dbo.Clients;"  -ServerInstance "$Server" -Username "$NewUserName" -Password "$NewPassword" -Database "$Database" -ErrorAction Stop
+    DEL "$path\Data\BulkImportNamesRunnable.sql"
+    Write-Host "Created."
+}
+catch{
+    Write-Error "Script error: $_" -EA Stop
+}
 
 
 Write-Host "Done."
