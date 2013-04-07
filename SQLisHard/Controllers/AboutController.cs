35a55f1ea67176bb8e839adb349b20f7fe73d814
Eli Weinstock-Herman <tarwn@tiernok.com>
2013-04-06 23:06:28 -0400

    Added About pages

---
+++ SQLisHard/Controllers/AboutController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SQLisHard.Controllers
+{
+    public class AboutController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult Exercises()
+		{
+			return View();
+		}
+
+		public ActionResult Tech()
+		{
+			return View();
+		}
+
+    }
+}
