067bdc071ca57e17821dd61367866c8aa0d8b782
Eli weinstock-Herman <eli@tiernok.com>
2026-06-11 18:24:16 -0400

    [fix] updates deps for audit fixes

--- src/e2e-tests/playwright.config.ts
+++ src/e2e-tests/playwright.config.ts
@@ -15,4 +15,9 @@ export default defineConfig({
     video: 'retain-on-failure',
     baseURL: process.env.BASE_URL || 'http://localhost:8012',
   },
+  webServer: {
+    command: "cd ../backend/SQLisHard && dotnet run --urls=http://localhost:8012 --environment=Test",
+    url: "http://localhost:8012",
+    reuseExistingServer: false
+  }
 }); 
